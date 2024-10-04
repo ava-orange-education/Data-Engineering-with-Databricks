7c24639267af66f40d15d3e9051ab368886372d8
AVA™ <102225901+OrangeAVA@users.noreply.github.com>
2024-10-04 14:55:42 +0530

    Published by Orange, AVA™

---
+++ Chapter 19/Models/vmQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class vmQuery
+    {
+        public string queryText { get; set; }
+    }
+}
